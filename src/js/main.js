c3f14c2e1f6702c911fdb236bd228cc4a7c66a1c
vitaliy <uryashnik@gmail.com>
2022-06-02 17:53:11 +0300

    add section before scheme

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const toggleIcon = document.getElementsByClassName('header__nav-icon')[0];
 const menu = document.getElementsByClassName('preview__adaptive-menu')[0];
 const rangeInput = document.getElementsByClassName('range__input')[0];
 const rangeResult = document.getElementsByClassName('range__result')[0];
+// const sliderButtons = document.getElementsByClassName('slider-nav')[0];
 
 toggleIcon.addEventListener('click', function (e) {
     menu.classList.toggle('preview__adaptive-menu_open');
@@ -10,3 +11,6 @@ toggleIcon.addEventListener('click', function (e) {
 rangeInput.addEventListener('change', function(){
     rangeResult.innerHTML = this.value;
 })
+
+// console.log(sliderButtons)
+// sliderButtons.classList.add("custom-nav-btm")
